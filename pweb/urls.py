66cb120cabb919e51a8e5deba8f6f6bc2d568eac
justwallysson <franciscowallyssonsilva@gmail.com>
2025-01-17 11:21:39 -0300

    a

--- pweb/urls.py
+++ pweb/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('sobre', views.sobre, name='sobre'),
+    path('contato', views.contato, name='contato'),
+    path('ajuda', views.ajuda, name='ajuda'),
 ]
+
